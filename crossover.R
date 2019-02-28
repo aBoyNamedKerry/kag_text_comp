22c9c6d72496c0de8bf1280761b7985d71a37d1a
kerry cella <kerry.cella@gmail.com>
2019-02-28 16:05:24 +0000

    Word count derived and method for deriving test crossover output and also test for lemmatization but not run.

--- crossover.R
+++ crossover.R
@@ -2,8 +2,10 @@
 
 
 #read in data
-data <- read.csv("./data/train_tokenized.csv",header=TRUE,
+data <- read.csv("./data/test.csv",header=TRUE,
                  stringsAsFactors = FALSE)
+#test
+data<- data[1:2345796,]
 
 #load libraries required
 library("wordnet")
@@ -12,10 +14,10 @@ library("tidytext")
 library("tm")
 library("textstem")
 library("tidyr")
+library(magrittr)
 
 data$question1 <- tolower(data$question1)
 data$question2 <- tolower(data$question2)
-
 
 data$question1 <- removePunctuation(data$question1)
 data$question2 <- removePunctuation(data$question2)
@@ -58,8 +60,8 @@ countOfSame_2 <- function(data)
 
 
 
-data_questions<-data[,5:6]
-Output$score<-apply(data_questions,1,countOfSame)
+#data_questions<-data[,5:6]
+#Output$score<-apply(data_questions,1,countOfSame)
 
 
 #Output$Crossover<-countOfSame_2(data)
@@ -75,19 +77,17 @@ corpus_q2<-Corpus(VectorSource(data_stopwords$question2))
 q1_tm<-tm_map(corpus_q1, removeWords, stopwords('english'))
 q2_tm<-tm_map(corpus_q2, removeWords, stopwords('english'))
 
-data_stopwords$question1 <- data.frame(text=sapply(q1_tm, identity), 
-                                       stringsAsFactors=F)
+data_stopwords$question1 <- unlist(q1_tm)[1:2345796]
 
-data_stopwords$question2 <-  data.frame(text=sapply(q2_tm, identity), 
-                                        stringsAsFactors=F)
+data_stopwords$question2 <-  unlist(q2_tm)[1:2345796]
 
-remove(corpus_q1,corpus_q2,q1_tm,q2_tm)
+remove(corpus_q1,corpus_q2,q1_tm,q2_tm);gc()
 
-data_stopwords$question1<-as.character(data_stopwords$question1)
-data_stopwords$question2<-as.character(data_stopwords$question2)
+#data_stopwords$question1<-as.character(data_stopwords$question1)
+#data_stopwords$question2<-as.character(data_stopwords$question2)
 
 
-data_questions_stopwords<-data_stopwords[,5:6]
+data_questions_stopwords<-data_stopwords[,2:3]
 #data$score_stopwords_removed<-apply(data_questions_stopwords,1,countOfSame)
 
 Output$Crossover_stopwords<-apply(data_questions_stopwords,1,countOfSame)
@@ -103,24 +103,45 @@ gc()
 #Stopwords remeoved and lemmatized
 
 data_lem<-data_stopwords
-rm(data_stopwords) ; gc()
+#rm(data_stopwords) ; gc()
 
-lem_q1<-as.vector(data_lem$question1)
-lem_q2<-as.vector(data_lem$question2)
+#lem_q1<-data$question1
+#lem_q2<-data_lem$question2
 
-data_lem$question1<-lemmatize_strings(lem_q1)
-data_lem$question2<-lemmatize_strings(lem_q2)
+data_lem$question1<-lemmatize_strings(data$question1)
+data_lem$question2<-lemmatize_strings(data$question2)
 
-Output$Crossover_stopwords<-apply(data_lem,1,countOfSame)
+data_questions_stopwords<-data_lem[,2:3]
 
-
-
-Output$Crossover_stopwords<-countOfSame_2(data_stopwords)
+Output$Crossover_stopwords_lim<-apply(data_questions_stopwords,1,countOfSame)
 
 
+
+## Add word count to test
+
+data %<>% mutate(q1_words_tot = sapply(strsplit(question1, " "), length),
+                 q2_words_tot = sapply(strsplit(question2, " "), length))
+
+
+#write out the data to test
+data %>% select(q1_words_tot, q2_words_tot) %>%
+  write.csv(., "./features_test/word_count_train.csv")
+
+
+## Test data
+ test_data <- head(data)
+ 
+ test_data %>% mutate(q1_words_tot = stringr::str_count(data$question1, "//w+"))
+ 
+ 
+ sapply(strsplit(test_data$question1, " "), length)
+ 
 ##test the lemmatization
 
 lem_q1_test <- lem_q1[1:10000]
 
 lem_sting_test<- lemmatize_strings(lem_q1_test)
 
+
+
+head(lem_q1)
