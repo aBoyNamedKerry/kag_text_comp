7f46870c9149598345fa3e16655f50966ac447b0
kerry cella <kerry.cella@gmail.com>
2019-02-28 10:04:59 +0000

    Added the sentiment creation and also a removal step to test to ave memory in R. This might not be needed depending on computer running code.

--- EDA.R
+++ EDA.R
@@ -86,24 +86,61 @@ round(prop.table(table(train_tokenized$same_sentiment,
       train_tokenized$sentiment_difference),1)*100,2)
 
 
-## create feature for test ---
+## create feature for test ==============================
 
 #filter to the n umber needed for submission 
 test_tokenized<- test_tokenized[0:2345796,]
 
 
-
+#create the nested word splits for doing the sentiment analysis
 test_tokenized_1 <- test_tokenized %>% 
-  # mutate(id = row_number()) %>%
   unnest_tokens(words_1, question1) 
 
 test_tokenized_2 <- test_tokenized %>% unnest_tokens(words_2, question2)
 
 
+#join to sentiment analysis list
+tokenized_sentiment1 <- test_tokenized_1  %>% inner_join(get_sentiments("bing"),
+                                                          by = c("words_1" = "word")) %>%
+  count(test_id, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative) %>%
+  mutate(overall_sentiment = ifelse(sentiment>0,"postive",
+                                    ifelse(sentiment == 0, "neutral", "negative")))
 
 
 
-## Testing code -----------------
+tokenized_sentiment2 <- test_tokenized_2  %>% inner_join(get_sentiments("bing"),
+                                                          by = c("words_2" = "word")) %>%
+  count(test_id, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative) %>%
+  mutate(overall_sentiment = ifelse(sentiment>0,"postive",
+                                    ifelse(sentiment == 0, "neutral", "negative")))
+
+
+#remove the data here as these are big data set
+
+rm(test_tokenized_1, test_tokenized_2)
+gc()
+
+## join the data --------------
+
+#join first sentiment
+test_tokenized <- tokenized_sentiment1 %>%
+  select(test_id, sentiment_q1 = sentiment, overall_sentiment_q1 = overall_sentiment) %>%
+  left_join(test_tokenized,.)
+
+#join second
+test_tokenized <- tokenized_sentiment2 %>%
+  select(test_id, sentiment_q2 = sentiment, overall_sentiment_q2 = overall_sentiment) %>%
+  left_join(test_tokenized,.)
+
+
+
+
+
+## Testing code =====================
 test <- train_tokenized_1  %>% left_join(get_sentiments("nrc"),
                                                           by = c("words_1" = "word")) %>%
   count(qid1, sentiment)
