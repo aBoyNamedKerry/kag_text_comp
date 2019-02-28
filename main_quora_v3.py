391e249417674f6c524bb1c6e7c0df1d9f504b09
AdrianJHam <48050872+AdrianJHam@users.noreply.github.com>
2019-02-28 14:11:21 +0000

    update run script and include features_test

--- main_quora_v3.py
+++ main_quora_v3.py
@@ -6,8 +6,12 @@ Created on Mon Feb 18 19:34:03 2019
 """
 import nltk
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier, AdaBoostClassifier
 from sklearn.model_selection import cross_val_score
+from sklearn.svm import LinearSVC
+
 from sklearn import tree
 import glob, os
 import pickle
@@ -19,7 +23,7 @@ tokenize_test = False
 build_train_features = False
 build_model = True
 build_test_features = False
-execute_model = False
+execute_model = True
 
 def tokenize(df, name):
     df['q1_tokens'] = df.apply(lambda row: nltk.word_tokenize(row['question1']), axis=1)
@@ -69,7 +73,8 @@ if build_train_features:
     create_features(df_train, 'features_train')
 
 if build_model:
-
+    if not build_train_features:
+        df_train = pd.read_csv('data/train_tokenized.csv', encoding = "ISO-8859-1")
     df_train_features = get_features_from_csv('features_train')
     
     X = df_train_features.values
@@ -79,26 +84,47 @@ if build_model:
     #print(y)
     
     #clf = tree.DecisionTreeClassifier(max_depth = 4)
-    clf = RandomForestClassifier(n_estimators=50, min_samples_leaf=500)
-      
-    scores = cross_val_score(clf, X, y, cv=2, scoring = 'neg_log_loss') #scoring = 'neg_log_loss',scoring = 'accuracy'
-   
-    print(scores)
+    #clf = RandomForestClassifier(n_estimators=50, min_samples_leaf=500) # -0.508
+    clf1 = LogisticRegression(solver='lbfgs',
+                              random_state=1)
+    clf2 = RandomForestClassifier(n_estimators=50, min_samples_leaf=500, random_state=1)
+    clf3 = GaussianNB()
+    clf4 = LinearSVC()
+    clf5 = AdaBoostClassifier()
+    clf_z = VotingClassifier(estimators=[
+            ('lr', clf1), ('rf', clf2), ('', clf3), ('svm', clf4)], voting='soft')
+
+    
+    # Try this on the model build - it will double the 
+    #class_weight={0: 2, 1: 1}
+    clf = RandomForestClassifier(n_estimators=50, min_samples_leaf=500, random_state=1)
+    
+    get_score=False
+    if get_score:
+        print("Running cross_val_score")
+        scores = cross_val_score(clf, X, y, cv=2, scoring = 'neg_log_loss') #scoring = 'neg_log_loss',scoring = 'accuracy'
        
+        print(scores)
+       
+    print('fitting model on all training data')
     clf.fit(X,y)
     
-    importances = clf.feature_importances_
-    std = np.std([tree.feature_importances_ for tree in clf.estimators_],
-             axis=0)
-    indices = np.argsort(importances)[::-1]
-
+    try:
+        importances = clf.feature_importances_
+        std = np.std([tree.feature_importances_ for tree in clf.estimators_],
+                 axis=0)
+        indices = np.argsort(importances)[::-1]
+        print("Feature ranking:")
+    
+        for f in range(X.shape[1]):
+            print("%d. feature %d %s (%f)" % (f + 1, indices[f], df_train_features.columns[indices[f]],importances[indices[f]]))
+        
+            
+    except:
+        print("Feature importances not available")
    
     
-    print("Feature ranking:")
 
-    for f in range(X.shape[1]):
-        print("%d. feature %d %s (%f)" % (f + 1, indices[f], df_train_features.columns[indices[f]],importances[indices[f]]))
-    
     # save the model to disk
     modelfilename = 'finalized_model.pkl'
     pickle.dump(clf, open(modelfilename, 'wb'))
@@ -125,19 +151,22 @@ if build_test_features:
         
     else:
         # read from csv already tokenized
-        df_test = pd.read_csv('data/test2_tokenized.csv', encoding = "ISO-8859-1")
+        print ('read_test_tokenized')
+        df_test = pd.read_csv('data/test2_tokenized.csv', dtype={"test_id": str, "question1": str,  "question2": str}, encoding = "ISO-8859-1")
 
     #df_test = df_test[0:100]
     #print(df_test)
+    print('start creating test features')
     create_features(df_test, 'features_test')
     
 if execute_model:
     if not build_test_features:
-        df_test = pd.read_csv('data/test.csv', dtype={"test_id": str, "question1": str,  "question2": str})
-
+        df_test = pd.read_csv('data/test2_tokenized.csv', dtype={"test_id": str, "question1": str,  "question2": str}, encoding = "ISO-8859-1")
+    df_test=df_test[0:2345796]
     clf = pickle.load(open(modelfilename, 'rb'))
+    print('getting features')
     df_test_features = get_features_from_csv('features_test')
-
+    df_test_features = df_test_features[0:2345796]
     X = df_test_features.values
     probs = clf.predict_proba(X)
     show_results=False
